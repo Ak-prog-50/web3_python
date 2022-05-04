165a5581deebbc9d08faf3b2a127517048494acf
Ak <akalankapathirage22@gmail.com>
2022-05-04 09:57:27 +0530

    connecting to ganache + printing nonce

--- deploy.py
+++ deploy.py
@@ -32,3 +32,13 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 abi = json.loads(
     compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
 )["output"]["abi"]
+
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 5777
+my_address = "0xA1c6bA7bCDFC20C2B09B8F09263bE19F0179044c"
+private_key = "1a1e0f4d00bd4b4f80e88c838f911baff104f55a4583201e6e602f5dd36136dd"
+
+nonce = w3.eth.getTransactionCount(my_address)
+
+# print(nonce)  nonce is the transaction count in this case.
+# print(dir(w3))
